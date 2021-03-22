913f2252d5a0a0a8d846bc0e457ca5f042a9aea0
Rui Braz Correia Junior <ruibraz@pop-os.localdomain>
2021-03-22 20:31:10 -0300

    primeiro commit

---
+++ src/main/kotlin/br/com/braz/rui/poccadadocodigo/PocCadaDoCodigoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.braz.rui.poccadadocodigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocCadaDoCodigoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocCadaDoCodigoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/braz/rui/poccadadocodigo/PocCadaDoCodigoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.braz.rui.poccadadocodigo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocCadaDoCodigoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
